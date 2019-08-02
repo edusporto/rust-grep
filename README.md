bc406d22b27971d8c853e925e2c48438ae8e5aba
Eduardo Sandalo Porto <30930225+edusporto@users.noreply.github.com>
2019-08-02 17:32:36 -0300

    Support for case insensitive search

--- README.md
+++ README.md
@@ -3,16 +3,17 @@ Recreating basic grep functionalities in Rust. Exercise part of The Rust Book.
 
 ## Building
 To build this program you will need to have installed the Rust Programming Language. For a tutorial on this, check https://www.rust-lang.org/tools/install.
+
 Running ```cargo build --release``` will build the binary in the ```target/release``` directory. Alternatively, you can build the binary and run it at the same time using ```cargo run --release```.
 
-
 ## Usage
-```./rust-grep [QUERY] [FILE NAME]```
+```./rust-grep query [file name]```
 
-* **[QUERY]** - The query that will be searched for in the specified stream.
-* **[FILE NAME]** - OPTIONAL - If specified, the program will search for the query in the given file. If not, the program will search the query in the terminal's standard output.
+* **query** - The query that will be searched for in the specified stream.
+* **[file name]** - OPTIONAL - If specified, the program will search for the query in the given file. If not, the program will search the query in the terminal's standard output.
+* If the **CASE_INSENSITIVE** environment variable is set, the program will search for the query as case insensitive.
 
 Examples:
 * ```./rust-grep "nevermore" theraven.txt ```
 * ```cat theraven.txt | ./rust-grep "nevermore"```
-Both examples will produce the same output.
+* ```CASE_INSENSITIVE=1 ./rust-grep raven theraven.txt```
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
 use std::fs;
 use std::io::{self, Read};
+use std::env;
 
 extern crate termcolor;
 use std::io::Write;
@@ -11,12 +12,13 @@ mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape.";
 
         let s: Vec<&str> = search(query, contents)
             .iter()
@@ -25,43 +27,82 @@ Pick three.";
 
         assert_eq!(vec!["safe, fast, productive."], s);
     }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        let s: Vec<&str> = search_case_insensitive(query, contents)
+            .iter()
+            .map(|a| a.line)
+            .collect();
+        
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            s
+        );
+    }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let content = if config.filename.is_empty() {
+    // 1) Read content to be searched
+    let content = read_content(&config)?;
+
+    // 2) Search the query in the specified content
+    let results = if config.case_sensitive {
+        search(&config.query, &content)
+    } else {
+        search_case_insensitive(&config.query, &content)
+    };
+
+    // 3) Print the lines from the content containing the query
+    print_results(&results, &config)?;
+
+    Ok(())
+}
+
+fn read_content(config: &Config) -> Result<String, io::Error> {
+    if config.filename.is_empty() {
         // if no file is specified, the program will read
         // from stdin until EOF
         let mut buffer = String::new();
         while io::stdin().read_to_string(&mut buffer)? != 0 {}
 
-        buffer
+        Ok(buffer)
     } else {
-        fs::read_to_string(&config.filename)?
-    };
+        fs::read_to_string(&config.filename)
+    }
+}
 
-    let results = search(&config.query, &content);
+fn print_results(results: &Vec<Match>, config: &Config) -> Result<(), Box<dyn Error>> {
+    let query_len = config.query.chars().count();
 
     let mut stdout = StandardStream::stdout(ColorChoice::Always);
     let standard_color = ColorSpec::new();
     stdout.set_color(&standard_color)?;
 
     for m in results {
-        write!(&mut stdout, "{}", &m.line()[0..m.index()])?;
+        write!(&mut stdout, "{}", &m.line[0 .. m.index])?;
         // writes everything up to the matched query
 
         stdout.set_color(ColorSpec::new().set_fg(Some(Color::Blue)))?;
         // sets the color to blue
 
-        write!(&mut stdout, "{}", &config.query)?;
+        write!(&mut stdout, "{}", &m.line[m.index .. m.index+query_len])?;
         // writes the matched query
 
         stdout.set_color(&standard_color)?;
         // sets the color back to default
 
         writeln!(&mut stdout, "{}",
-                 &m.line()[m.index()+config.query.chars().count()..])?;
+                 &m.line[m.index+config.query.chars().count() ..])?;
         // writes everything past the matched query
-    }        
+    }
 
     Ok(())
 }
@@ -78,9 +119,23 @@ pub fn search<'a>(query: &str, content: &'a str) -> Vec<Match<'a>> {
     results
 }
 
+pub fn search_case_insensitive<'a>(query: &str, content: &'a str) -> Vec<Match<'a>> {
+    let mut results = Vec::new();
+    let query = query.to_lowercase();
+
+    for line in content.lines() {
+        if let Some(index) = line.to_lowercase().find(&query) {
+            results.push(Match { line, index });
+        }
+    }
+
+    results
+}
+
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -92,26 +147,13 @@ impl Config {
 
         let query = args.next().ok_or("Missing query argument")?;
         let filename = args.next().unwrap_or(String::new());
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
-        Ok(Config { query, filename })
+        Ok(Config { query, filename, case_sensitive })
     }
 }
 
 pub struct Match<'a> {
-    line: &'a str,
-    index: usize,
-}
-
-impl<'a> Match<'a> {
-    pub fn new(line: &'a str, index: usize) -> Match<'a> {
-        Match {line, index}
-    }
-
-    pub fn line(&self) -> &'a str {
-        self.line
-    }
-
-    pub fn index(&self) -> usize {
-        self.index
-    }
+    pub line: &'a str,
+    pub index: usize,
 }
